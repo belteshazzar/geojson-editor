16a6dff297cc2d286eb3678920fcbd2fb4dfac6f
Daniel Walton <daniel@belteshazzar.com.au>
2023-03-20 23:33:59 +1030

    small updates, working on geojson merge for regions

--- scripts/merge_shared_boundary.js
+++ scripts/merge_shared_boundary.js
@@ -14,17 +14,18 @@ const geojson2 = JSON.parse(fs.readFileSync(`${dirname}/${r2}/${r2}_${y2}.geojso
 
 geojson2.geometry.coordinates[0] = geojson2.geometry.coordinates[0].reverse()
 
-// find shared coords
+// find shared edge
 
 for (let i1 = 0 ; i1< geojson1.geometry.coordinates[0].length ; i1++) {
     let c1 = geojson1.geometry.coordinates[0][i1]
 
     for (let i2 = 0 ; i2 < geojson2.geometry.coordinates[0].length ; i2++) {
         let c2 = geojson2.geometry.coordinates[0][i2]
+
         if (c1[0] == c2[0] && c1[1] == c2[1]) {
             console.log(i1,i2)
             break
         }
     }
-
 }
+
