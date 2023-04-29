45df8910708231d5c3aed094725761da1584c28f
Daniel Walton <daniel@belteshazzar.com.au>
2023-04-29 20:50:21 +0930

    post- actual single year conversion

--- scripts/single-year.js
+++ scripts/single-year.js
@@ -35,32 +35,44 @@ fs.readdirSync(dirname, {withFileTypes: true})
             if (geojson.properties.label.lat == NaN) geojson.properties.label.lat = 0
             if (geojson.properties.label.lng == NaN) geojson.properties.label.lng = 0
 
-            if ( i<years.length - 1 
-                && Number.parseInt(geojson.properties.year.to) + 1 < years[i+1]) {
+            if (geojson.properties.year.to == undefined || geojson.properties.year.from == undefined) {
+                geojson.properties.year = { from: geojson.properties.year, to : geojson.properties.year }
+            }
 
-                    console.log(i,years[i],geojson.properties.year.from,geojson.properties.year.to, i<years.length-1 ? years[i+1]:'-')
+            geojson.properties.year.from = Number.parseInt(geojson.properties.year.from)
+            geojson.properties.year.to = Number.parseInt(geojson.properties.year.to)
 
-                    const createYear = Number.parseInt(geojson.properties.year.to) + 1
-                    const createFilename = `${dirname}/${region}/${region}_${createYear}.geojson`
-                    const createGeojson = {
-                        "type": "Feature",
-                        "properties": {
-                            "name": region,
-                            "year": createYear,
-                            "known_as": geojson.properties['known_as'],
-                            "source": "",
-                            "overlay": {
-                                "url": "",
-                                "c1": { "lat":0,"lng":0},
-                                "c2": {"lat":0,"lng":0}
-                            },
-                            "note":"",
-                            "label": geojson.properties.label,
-                            "geometry": null
-                        }
+            function writeNull(atYear) {
+                console.log(`i=${i} year=${years[i]} from=${geojson.properties.year.from} to=${geojson.properties.year.to} next=${i==years.length-1 ? '-' : years[i+1]} null=${atYear}`)
+
+                const createFilename = `${dirname}/${region}/${region}_${atYear}.geojson`
+                const createGeojson = {
+                    "type": "Feature",
+                    "properties": {
+                        "name": region,
+                        "year": atYear,
+                        "known_as": geojson.properties['known_as'],
+                        "source": "",
+                        "overlay": {
+                            "url": "",
+                            "c1": { "lat":0,"lng":0},
+                            "c2": {"lat":0,"lng":0}
+                        },
+                        "note":"",
+                        "label": geojson.properties.label,
+                        "geometry": null
                     }
+                }
 
-                    fs.writeFileSync(createFilename, JSON.stringify(createGeojson))
+                fs.writeFileSync(createFilename, JSON.stringify(createGeojson))
+
+            }
+
+            if ( i == years.length - 1 && geojson.properties.year.to < 2022) {
+                writeNull(geojson.properties.year.to+1)
+            } else if (i<years.length - 1 
+                && geojson.properties.year.to + 1 < years[i+1]) {
+                writeNull(geojson.properties.year.to+1)
             }
 
             geojson.properties.year = years[i]
