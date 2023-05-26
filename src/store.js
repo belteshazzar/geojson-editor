8f16e8270c9ed6f5e483c28285fc7daf3e1bec44
Daniel Walton <daniel@belteshazzar.com.au>
2023-05-26 20:12:23 +0930

    1200AD

--- src/store.js
+++ src/store.js
@@ -26,7 +26,7 @@ export const store = createStore({
         //   lng: -12.656121191677354
         // }
       },
-      displayYear: 1099,
+      displayYear: 1199,
       loading: true,
       region: {
         type: 'Feature',
