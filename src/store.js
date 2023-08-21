c67f77ffe8fffbdbf5d854dbb536eb1207fa2caa
Daniel Walton <daniel@belteshazzar.com.au>
2023-08-21 19:50:37 +0930

    asia fixed to 1042

--- src/store.js
+++ src/store.js
@@ -26,7 +26,7 @@ export const store = createStore({
         //   lng: -12.656121191677354
         // }
       },
-      displayYear: 999,
+      displayYear: 1041,
       loading: true,
       region: {
         type: 'Feature',
